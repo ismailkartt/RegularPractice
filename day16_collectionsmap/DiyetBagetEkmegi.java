434b0eabb65fee60e97a0c3a4ed58dd4e5dbcae7
Ismail Kart <ismailkartt@gmail.com>
2022-12-31 21:43:27 +0300

    31/12/2022

---
+++ day16_collectionsmap/DiyetBagetEkmegi.java
@@ -0,0 +1,6 @@
+package day16_collectionsmap;
+
+public class DiyetBagetEkmegi extends BagetEkmek{
+    DiyetBagetEkmegi diybageEkm= new DiyetBagetEkmegi();
+    BagetEkmek baget=new BagetEkmek();
+}
---
+++ day16_collectionsmap/Level.java
@@ -0,0 +1,7 @@
+package day16_collectionsmap;
+
+public enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
